38b26a7a9a40de84eba9a1c2b16dd99d83341c81
mdsurfer <nerik85@gmail.com>
2019-06-25 10:22:42 +0300

    final fix

--- app.js
+++ app.js
@@ -6,21 +6,23 @@ let appData = {
         budget: money,
         date: time,
         expenses: {
-            // costUnitOne: '',
-            // costUnitTwo: ''
+
         },
-        optionalExpenses: {},
-        income: [],
+        optionalExpenses: {
+
+        },
+        income: [
+
+        ],
         savings: false
     };
-// console.log(money, time);
-// console.log(appdata);
-// let qOne = appData.expenses['costUnitOne'];
-// let qTwo = appData.expenses['costUnitTwo'];
+
 let qOne = prompt('Введите обязательную статью расходов в этом месяце');
 let aOne = prompt('Во сколько обойдется?');
 let qTwo = prompt('Введите обязательную статью расходов в этом месяце');
 let aTwo = prompt('Во сколько обойдется?');
+
+
 appData.expenses[qOne] = aOne;
 appData.expenses[qTwo] = aTwo;
 
@@ -31,3 +33,4 @@ function dailyBudget() {
     let averageBudget = (aOne + aTwo)/30;
     return averageBudget;
 }
+

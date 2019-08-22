00c438e7f8e6c0174b20bea72ae939acff3fd16b
mdsurfer <nerik85@gmail.com>
2019-08-22 22:09:48 +0300

    hometask 8

--- home-task-8/js/app.js
+++ home-task-8/js/app.js
@@ -2,12 +2,12 @@ window.addEventListener('DOMContentLoaded', function() {
 
     'use sctrict';
     console.log('start');
-    let tab = document.querySelectorAll('.info-header-tab'),
+    var tab = document.querySelectorAll('.info-header-tab'),
         info = document.querySelector('.info-header'),
         tabContent = document.querySelectorAll('.info-tabcontent');
 
     function hideTabContent(a) {
-        for (let i = a; i < tabContent.length; i++) {
+        for (var i = a; i < tabContent.length; i++) {
             tabContent[i].classList.remove('show');
             
             tabContent[i].classList.add('hide');
@@ -23,9 +23,9 @@ window.addEventListener('DOMContentLoaded', function() {
     }
 
     info.addEventListener('click', function(event) {
-        let target = event.target;
+        var target = event.target;
         if (target && target.classList.contains('info-header-tab')) {
-            for( let i = 0; i < tab.length; i++) {
+            for( var i = 0; i < tab.length; i++) {
                 if (target == tab[i]) {
                     hideTabContent(0);
                     showTabContent(i);
@@ -35,3 +35,89 @@ window.addEventListener('DOMContentLoaded', function() {
         }
     });
 });
+
+
+// Timer
+
+var deadline = '2019-07-07';
+
+function getTimeRemaining(endtime) {
+    var t = Date.parse(endtime) - Date.parse(new Date()),
+        seconds = Math.floor((t/1000)%60),
+        minutes = Math.floor((t/1000/60)%60),
+        hours = Math.floor((t/(1000*60*60)));
+        
+    if ( t <= 0 ) {
+        return {
+            'total' : t,
+            'hours' : '00',
+            'minutes' : '00',
+            'seconds' : '00'
+        };
+    }
+    else { 
+        return {
+        'total' : t,
+        'hours' : hours,
+        'minutes' : minutes,
+        'seconds' : seconds
+    };
+    }
+}
+
+function drawTimer(id, endtime) {
+    var timer = document.getElementById(id),
+        hours = timer.querySelector('.hours'),
+        minutes = timer.querySelector('.minutes'),
+        seconds = timer.querySelector('.seconds');
+        timeInterval = setInterval(updateTime, 1000);
+    
+    function updateTime() {
+        var t = getTimeRemaining(endtime); 
+
+        if (t.hours < 10) {
+            hours.textContent =  '0' + t.hours;
+        } else {
+            hours.textContent = t.hours;
+        }
+        if (t.hours < 10) {
+            minutes.textContent = '0' + t.minutes;
+            } else {
+                minutes.textContent = t.minutes;
+            }
+
+        if (t.seconds < 10) {
+                seconds.textContent =  '0' + t.seconds;
+
+            } else {
+                seconds.textContent = t.seconds;
+            }
+    }
+}
+
+drawTimer('timer', deadline);
+
+let tabBtns = document.querySelectorAll('.description-btn'),
+    moreBtn = document.querySelector('.more'),
+    overlay = document.querySelector('.overlay'),
+    close = document.querySelector('.popup-close');
+   
+function openModalPopup(tab) {
+        overlay.style.display = 'block';
+        //this.classList.add('more-splash');
+}
+
+
+    close.addEventListener("click", function() {
+        overlay.style.display = 'none';
+    });
+//}
+
+tabBtns.forEach(function(btn, i) {
+    btn.addEventListener("click", function() {
+        openModalPopup(tabBtns);
+    });
+});
+
+
+
---
+++ home-task-8/js/hw.js
@@ -0,0 +1,7 @@
+let age = document.getElementById('age');
+let name ='dd';
+let surname ='dd';
+function showUser() {
+	alert("Пользователь " + surname + " " + name + ", его возраст " + age.value);
+}
+showUser();
